e931b1691844c56dfe71623cddec49a90937fd53
Paul Musgrave <ps.musgrave@gmail.com>
2018-11-28 13:32:03 -0500

    initial working version, outputs correct matrix might have memory limitations, only tested with small DOT files so far

--- dotmatrix.js
+++ dotmatrix.js
@@ -4,7 +4,7 @@ const dot_path = process.argv[2];
 
 let read_stream = fs.createReadStream(dot_path).setEncoding('ascii');
 
-let edges = [0];
+let edges = new Array();
 let adjacency_list = {};
 
 const rl = readline.createInterface({
@@ -20,20 +20,63 @@ rl.on('line', (line) => {
 
 rl.on('close', () => {
   edges.sort();
+  // console.log('edges:\n', edges)
+  let vertices = new Array();
 
   for (let i = 0; i < edges.length; i++){
-    if(adjacency_list[edges[i][0]] == undefined){
-      adjacency_list[edges[i][0]] = new Array(edges[i][1]);
+    let vertex_left = edges[i][0].replace(';', '');
+    let vertex_right = edges[i][1].replace(';', '');
+
+    if (!vertices.includes(vertex_left)){
+      vertices.push(vertex_left);
+    }
+    if (!vertices.includes(vertex_right)){
+      vertices.push(vertex_right);
+    }
+
+    if(adjacency_list[vertex_left] == undefined){
+      adjacency_list[vertex_left] = new Array(vertex_right);
     }
     else {
-      adjacency_list[edges[i][0]].push(edges[i][1]);
+      adjacency_list[vertex_left].push(vertex_right);
+    }
+
+    if(adjacency_list[vertex_right] == undefined){
+      adjacency_list[vertex_right] = new Array();
+    }
+    // else {
+    //   adjacency_list[vertex_right].push();
+    // }
+  }
+
+  // console.log('adj_list: ', adjacency_list);
+  // console.log('vertices: ', vertices.sort());
+
+  // let vertices = Object.keys(adjacency_list);
+  vertices = vertices.sort();
+  let num_unique_vertices = vertices.length;
+  let adjacency_matrix = new Array(num_unique_vertices);
+  for (let i = 0; i < num_unique_vertices; i++){
+    adjacency_matrix[i] = (new Array(num_unique_vertices));
+  }
+
+  for (let i = 0; i < num_unique_vertices; i++){
+    for (let j = 0; j < num_unique_vertices; j++){
+      adjacency_matrix[i][j] = 0;
     }
   }
 
-  let num_unique_vertices = Object.keys(adjacency_list).length;
-  let adjacency_matrix = [num_unique_vertices][num_unique_vertices];
+  for (let i = 0; i < num_unique_vertices; i++){
+    for (let j = 0; j < num_unique_vertices; j++){
+      let current_vertex = vertices[i];
 
-  for (let j = 0; j < num_unique_vertices; j++){
-    
+      // console.log('current: ', current_vertex, '\tadj_list[current]: ', adjacency_list[current_vertex]);
+
+      adjacency_list[current_vertex].forEach((vertex) => {
+        adjacency_matrix[i][vertices.indexOf(vertex)] = 1;
+      });
+    }
   }
+
+  console.log(adjacency_matrix);
 });
