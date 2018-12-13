ef1f1038842ff56d12b5e8743a0f2aa27b5dd7d3
pmusgrave <ps.musgrave@gmail.com>
2018-12-13 00:09:57 -0500

    refactored and linted

--- dotmatrix.js
+++ dotmatrix.js
@@ -1,57 +1,32 @@
 const fs = require('fs');
 const readline = require('readline');
 
-module.exports = function()
-{
+module.exports = (function () {
   this.edges = new Array();
   this.adjacency_list = {};
 
-  function convert_dot_file_to_matrix(path, callback){
-    this.read_stream = fs.createReadStream(path).setEncoding('ascii');
-
-    const rl = readline.createInterface({
-      input: this.read_stream,
-    });
-
-    rl.on('line', (line) => {
-      if (line.includes('->')){
-        edges.push(line.split(' -> '));
-      }
-      if (line.includes('--')){
-        edges.push(line.split(' -- '));
-      }
-    });
-
-    rl.on('close', () => {
-      let vertices = convert_edges_to_adjacency_list(edges);
-      adjacency_matrix = convert_list_to_matrix(vertices);
-      callback(adjacency_matrix);
-    });
-  }
-
-  function convert_edges_to_adjacency_list(edges){
+  function convert_edges_to_adjacency_list(edges) {
     edges.sort();
-    let vertices = new Array();
+    const vertices = new Array();
 
-    for (let i = 0; i < edges.length; i++){
-      let vertex_left = edges[i][0].replace(';', '');
-      let vertex_right = edges[i][1].replace(';', '');
+    for (let i = 0; i < edges.length; i++) {
+      const vertex_left = edges[i][0].replace(';', '');
+      const vertex_right = edges[i][1].replace(';', '');
 
-      if (!vertices.includes(vertex_left)){
+      if (!vertices.includes(vertex_left)) {
         vertices.push(vertex_left);
       }
-      if (!vertices.includes(vertex_right)){
+      if (!vertices.includes(vertex_right)) {
         vertices.push(vertex_right);
       }
 
-      if(this.adjacency_list[vertex_left] == undefined){
+      if (this.adjacency_list[vertex_left] == undefined) {
         this.adjacency_list[vertex_left] = new Array(vertex_right);
-      }
-      else {
+      } else {
         this.adjacency_list[vertex_left].push(vertex_right);
       }
 
-      if(this.adjacency_list[vertex_right] == undefined){
+      if (this.adjacency_list[vertex_right] == undefined) {
         this.adjacency_list[vertex_right] = new Array();
       }
     }
@@ -60,21 +35,21 @@ module.exports = function()
     return vertices;
   }
 
-  function convert_list_to_matrix(list){
-    let num_unique_vertices = list.length;
-    let adjacency_matrix = new Array(num_unique_vertices);
-    for (let i = 0; i < num_unique_vertices; i++){
-      adjacency_matrix[i] = (new Array(num_unique_vertices));
+  function convert_list_to_matrix(list) {
+    const num_unique_vertices = list.length;
+    const adjacency_matrix = new Array(num_unique_vertices);
+    for (let i = 0; i < num_unique_vertices; i++) {
+      adjacency_matrix[i] = new Array(num_unique_vertices);
     }
 
-    for (let i = 0; i < num_unique_vertices; i++){
-      for (let j = 0; j < num_unique_vertices; j++){
+    for (let i = 0; i < num_unique_vertices; i++) {
+      for (let j = 0; j < num_unique_vertices; j++) {
         adjacency_matrix[i][j] = 0;
       }
     }
 
-    for (let i = 0; i < num_unique_vertices; i++){
-      let current_vertex = list[i];
+    for (let i = 0; i < num_unique_vertices; i++) {
+      const current_vertex = list[i];
       this.adjacency_list[current_vertex].forEach((vertex) => {
         adjacency_matrix[i][list.indexOf(vertex)] = 1;
       });
@@ -83,8 +58,30 @@ module.exports = function()
     return adjacency_matrix;
   }
 
+  function convert_dot_file_to_matrix(path, callback) {
+    this.read_stream = fs.createReadStream(path).setEncoding('ascii');
+
+    const rl = readline.createInterface({
+      input: this.read_stream
+    });
+
+    rl.on('line', (line) => {
+      if (line.includes('->')) {
+        edges.push(line.split(' -> '));
+      }
+      if (line.includes('--')) {
+        edges.push(line.split(' -- '));
+      }
+    });
+
+    rl.on('close', () => {
+      const vertices = convert_edges_to_adjacency_list(edges);
+      const adjacency_matrix = convert_list_to_matrix(vertices);
+      callback(adjacency_matrix);
+    });
+  }
 
   return {
-    convert_dot_file_to_matrix: convert_dot_file_to_matrix,
+    convert_dot_file_to_matrix: convert_dot_file_to_matrix
   };
-}();
+}());
